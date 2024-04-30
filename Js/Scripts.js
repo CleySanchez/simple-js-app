6e2ec90cb103c87e1a018fe85475a9410d212123
CleySanchez <clement.sanchez.31@gmail.com>
2024-04-30 19:16:03 +0200

    pokemon repository
    Exercise 1.5

--- Js/Scripts.js
+++ Js/Scripts.js
@@ -1,30 +1,48 @@
-let pokemonList = [
-    {
-        name: "Pikachu ",
-        height: 2,
-        types: ["electric"]
-    },
-    {
-        name: "Charmander ",
-        height: 6,
-        types: ["fire"]
-    },
-    {
-        name: "Squirtle",
-        height: 5,
-        types: ["water"]
-    }
-];
+// Wrap pokemonList array in an IIFE to avoid global state access
+let pokemonRepository = (function() {
+    // Initialize an empty array to store Pokémon objects
+    let pokemonList = [
+        {
+            name: "Pikachu",
+            height: 2,
+            types: ["electric"]
+        },
+        {
+            name: "Charmander",
+            height: 6,
+            types: ["fire"]
+        },
+        {
+            name: "Squirtle",
+            height: 5,
+            types: ["water"]
+        }
+    ];
 
-console.log(pokemonList);
+    // Public functions to access and modify the pokemonList array
+    return {
+        getAll: function() {
+            return pokemonList;
+        },
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        }
+    };
+})();
 
-
-for (let i = 0; i < pokemonList.length; i++) {
-    const pokemon = pokemonList[i];
-    
+// Use a forEach() loop to iterate over the Pokémon in the pokemonList array and print their details
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(`${pokemon.name} (height: ${pokemon.height})`);
-    
     if (pokemon.height > 5) { 
         document.write(" - Wow, that's big!"); 
     }
+});
+
+// Function to print a message
+function printMessage() {
+    document.write("<p>CATCH THEM ALL !!!</p>");
 }
+
+// Call the printMessage function
+printMessage();
+
