0b80cc9a3d2e2b87b44673666eddb86b5caa549a
CleySanchez <clement.sanchez.31@gmail.com>
2024-05-07 17:15:20 +0200

    Mew pokemon added
    Mew 151

--- Js/Scripts.js
+++ Js/Scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
     async function add(pokemon) {
         pokemonList.push(pokemon);
